0245e602533fb8ab45054b09d7d949ad58f5ad72
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-10-30 10:04:17 +0200

    First commit - base of flights microservice

---
+++ src/main/java/ro/hackaville/wsflights/WsFlightsApplication.java
@@ -0,0 +1,13 @@
+package ro.hackaville.wsflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsFlightsApplication.class, args);
+	}
+
+}
